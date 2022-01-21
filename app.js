a366c40dd60af8ff3ac0e83462b376bea34fd421
Nichole McGrew <nicholemcgrew@gmail.com>
2022-01-21 14:45:41 -0600

    initial commit

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+
+let watching = new Promise((resolve, reject) => {
+    let userWatchingLiveStream = true
+    if (userWatchingLiveStream) {
+        resolve("Thumbs up and Subscribe!");
+    } else {
+        reject("User left.")
+    }
+})
+.then((result) => {
+    console.log(result);
+})
+.catch((err) => {
+    console.error(err);
+});
